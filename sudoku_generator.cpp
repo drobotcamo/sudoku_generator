61c2c810024429cae107fb0635e345ae19a9cf8e
Cameron Greene <cam.greene@gmail.com>
2022-07-27 17:45:14 -0700

    Modified recursor function and removePoss function
    deleted isPresent
    restructured recursor
    currently doesnt work

--- sudoku_generator.cpp
+++ sudoku_generator.cpp
@@ -39,15 +39,17 @@ void addToList(Node *head, int data){
 
 //deleteNode will take in a list and a data point, then find and delete that data point from the list
 //if the node is not present, then nothing will happen.
-void deleteNode(Node *head, int data){
+//will return a boolean depending whether or not deletion was successful
+bool deleteNode(Node *head, int data){
     Node *cursor = head;
     while((cursor->next != NULL) && (cursor->next->data != data)){
         cursor = cursor->next;
     }
     if(cursor->next == NULL)
-        return;
+        return false;
     cursor->next = cursor->next->next;
     head->data--;
+    return true;
 }
 
 //printList takes in a linked list as a parameter and prints it out in order to the terminal
@@ -63,6 +65,7 @@ void printList(Node *list){
 }
 
 //getData takes in a list and an index, and will return the data of the node at that index
+//index means that 
 int getData(Node *head, int index){
     Node *cursor = head;
     if(cursor->next == NULL)
@@ -110,24 +113,38 @@ void initializeArray(square **x){
 
 
 //given coordinates of a tile, this will eliminate the possibility that any tile in the same column row or box will share its value
-void adjustPossibilities(square **tiles, int x, int y){
+//returns a boolean that will tell whether a box now has 0 possibilities.
+//if a box now has 0 poss, then it will return false, and will restore all of the boxes that it had previously changed 
+bool removePossibilites(square **tiles, int x, int y){
+    bool succeeding = true;
+    int val = tiles[x][y].value, saveX, saveY;
+    Node *changed = makeList();
     for(int i = 0; i < 9; i++){
         for(int j = 0; j < 9; j++){
-            if((i == x | j == y) | (tiles[x][y].id == tiles[i][j].id))      
-                deleteNode(tiles[i][j].possibilities, tiles[x][y].value);   //delete the node from the possibilities list that matches the value of the tile
+            if((i == x | j == y) | (tiles[x][y].id == tiles[i][j].id)){   
+                if(deleteNode(tiles[i][j].possibilities, val))   //delete the node from the possibilities list that matches the value of the tile, assigns the bool to didChange
+                    addToList(changed, (i * 9) + j);
+                if(tiles[i][j].possibilities->data == 0 && tiles[i][j].value == 0){
+                    succeeding = false;
+                    saveX = i, saveY = j;
+                }
+            }
         }
     }
-}
 
-//isPresent will take in a list and a value, and return true if any of the nodes in that list contain the value. currently unused.
-bool isPresent(Node *head, int value){
-    Node *cursor = head->next;
-    while(cursor->next != NULL){
-        if(cursor->data == value)
-            return true;
-        cursor = cursor->next;
+    if(succeeding == false){
+        int k, j, key;
+        cout << "clearing the changes made by tile at x: " << y << ", y: " << x << ", whose value is: " << val << "\n";
+        cout << "the problem tile is at position x:" << saveY << ", y:" << saveX << "\n";
+        // while(changed->next != NULL){
+        //     key = changed->next->data;
+        //     k = key / 9;                        
+        //     j = key % 9; 
+        //     addToList(tiles[k][j].possibilities, val);
+        //     deleteNode(changed, key);
+        // }
     }
-    return false;
+    return succeeding;
 }
 
 //recursor is the engine that carries out one step of the creation process
@@ -135,29 +152,73 @@ bool isPresent(Node *head, int value){
 //then if there are possibilities in that tile's list, it will insert a random possible value
 //finally it will then adjust the possibilities of all other tiles on the board as such.
 //if there are no possibilities, then it will output "failed"
-void recursor(square **tiles, int counter, Node *openSpaces){
+void recursor(square **tiles, int *counter, int openSpaces[81], bool *lastWasSuccessful, bool *complete){
     int x, y, key, insert = 0;
-    key = getData(openSpaces, (rand() % openSpaces->data));
-    x = key / 9;
-    y = key % 9;
+    key = openSpaces[*counter];          //this code block gets the ID from the openspaces list, and translates it into x and y
+    x = key / 9;                        //
+    y = key % 9;                        //
     
-    if(tiles[x][y].possibilities->data != 0)
-        insert = getData(tiles[x][y].possibilities, (rand() % tiles[x][y].possibilities->data) + 1);
-    else
-        cout << "failed insertion\n";
-
-    deleteNode(openSpaces, key);
-    tiles[x][y].value = insert;
-    adjustPossibilities(tiles, x, y);
+    if(*counter == 81)
+    {                         
+        *complete = true;
+        return;
+    }
+    
+    while(tiles[x][y].possibilities->data > 0  && *complete == false){
+        cout << "we tried once: #" << *counter << "\n";
+        tiles[x][y].value = getData(tiles[x][y].possibilities, (rand() % tiles[x][y].possibilities->data) + 1);
+        if(removePossibilites(tiles, x, y) != false && lastWasSuccessful){
+            *counter = *counter + 1;
+            recursor(tiles, counter, openSpaces, lastWasSuccessful, complete);
+            *lastWasSuccessful = true;
+        }
+        else{
+            deleteNode(tiles[x][y].possibilities, tiles[x][y].value);
+            tiles[x][y].value = 0;
+        }
+    }
+    return;
 }
+
+
+    // if(lastWasSuccessful){       //there can be an insertion, and the last was successful
+    //     tiles[x][y].value = getData(tiles[x][y].possibilities, (rand() % tiles[x][y].possibilities->data) + 1);
+    //         //the line above will insert a random possibility, and then will adjust the other possibilities accordingly.
+    //     cout << "attempt #" << counter << "\n";
+    //         //now, if that adjustment left any empty tiles with 0 remaining possibilities, then we have a problem.
+
+    //     if(removePossibilites(tiles, x, y) == false){
+    //         cout << "and we just inserted a value at x:" << y << ", y:" << x << ", whose value is: " << tiles[x][y].value << "\n";
+    //         lastWasSuccessful = false;
+    //         tiles[x][y].value = 0;
+            
+    //     }
+    //     else{     
+    //         counter++;
+    //         recursor(tiles, counter, openSpaces, lastWasSuccessful, complete);
+    //     }
+    // }
 
 //createGame is the driver that calls recursor
 //currently will attempt a recursor until the board is determined to be unfillable 
 //(i.e. there are no possibilities in the remaining squares)
-void createGame(square **x, Node *openSpaces){
-    int count = 1;
-    while(openSpaces->data > 0) 
-        recursor(x, count, openSpaces);
+void createGame(square **x, int openSpaces[81]){
+    int count = 0;
+    bool lastWasSuccessful = true, complete = false;
+    recursor(x, &count, openSpaces, &lastWasSuccessful, &complete);
+}
+
+void randomSequence(int openSpaces[81]){
+    Node *available = makeList();
+    int key;
+    for(int i = 0; i < 81; i++){
+        addToList(available, i);
+    }
+    for(int i = 0; i < 81; i++){
+        key = (rand() % available->data);
+        openSpaces[i] = getData(available, key);
+        deleteNode(available, openSpaces[i]);
+    }
 }
 
 //debugger that allows the user to see the possibilities of a selected square
@@ -175,10 +236,12 @@ void debug(square **tiles){
         cout << "another? (y/n): ";
         char input;
         cin >> input;
-        if(input == 'y')
-            again = true;
-        else
+        if(input == 'n')
             again = false;
+        else if(input == 'q')
+            return;
+        else
+            again = true;
 
     } while(again);
 }
@@ -187,16 +250,17 @@ int main() {
     srand (time(NULL));                 //initialize randomizer
     square **tiles = new square*[9];    //initialize 2D array of squares
     for (int i = 0; i < 9; i++) 
-        tiles[i] = new square[9];       
-    Node *openSpaces = makeList();      //create openSpaces list
-    for(int i = 0; i < 81; i++)
-        addToList(openSpaces, i);       //fill openSpaces list
+        tiles[i] = new square[9];      
+
+    int openSpaces[81];
+    randomSequence(openSpaces);
 
     initializeArray(tiles);             //fills each square with placeholder value and possibilities
     createGame(tiles, openSpaces);      //fills the board with values
-    printBoard(tiles);                  //prints the board
+    printBoard(tiles);                   //prints the board
+    
 
-    printList(openSpaces);              //for debugging, print out the openSpaces list
+    //printList(openSpaces);              //for debugging, print out the openSpaces list
     //debug(tiles);
     
     for(int i=0;i<9;i++)                //delete the inner arrays
